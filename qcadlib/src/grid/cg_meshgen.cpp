80ac56fc8589415e412ad953669e595f981c6cb6
U-USTC-ATMEL\Administrator <Administrator@ustc-atmel.(none)>
2009-02-15 11:20:48 +0800

    win32 fix

--- qcadlib/src/grid/cg_meshgen.cpp
+++ qcadlib/src/grid/cg_meshgen.cpp
@@ -70,6 +70,7 @@ void MeshGenerator::do_mesh(const QString &cmd, bool enable_quadtree)
   {
     RS_DIALOGFACTORY->commandMessage(("Create QuadTree on PSLG..."));
     quadtree = build_quadtree();
+    quadtree->export_quadtree("aa.vtk"); 
   }
 
   //set point
